e1703c6e7bf17feeefb3e2d031f659e1cc4e0ce3
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-04-13 16:20:10 -0700

    Added some tests adding travis.yml for travis maybe

--- app/src/androidTest/java/com/example/tatapi/MyInstrumentedUnitTests.java
+++ app/src/androidTest/java/com/example/tatapi/MyInstrumentedUnitTests.java
@@ -1,26 +1,32 @@
 package com.example.tatapi;
 
 import android.content.Context;
+import android.util.Log;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class MyInstrumentedUnitTests {
+
+    @Test
+    public void intentFactoryCheck(){
+        boolean allIntentsPassing = true;
+        // for each activity, grab an activity
+        // if intent wasn't grabbed, allIntentPassing = false; break;
+        assertEquals(true, allIntentsPassing);
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.tatapi", appContext.getPackageName());
     }
+
 }
---
+++ app/src/test/java/com/example/tatapi/MyUnitTests.java
@@ -0,0 +1,13 @@
+package com.example.tatapi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyUnitTests {
+
+    @Test
+    public void mathIsCorrect(){
+        assertEquals(4, 2 + 2);
+    }
+}
